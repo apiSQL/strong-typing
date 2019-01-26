ce4fb6a85f4c7c3baed15a6a0c761b3b02c6e5c6
tom <tom@sapletta.com>
2019-01-26 10:01:51 +0100

    Update source

---
+++ src/Model/Holiday.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tom
+ * Date: 26.01.2019
+ * Time: 09:46
+ */
+
+namespace Phpfunc\Model;
+
+
+class Holiday
+{
+
+}
---
+++ src/Operation/Delete.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tom
+ * Date: 26.01.2019
+ * Time: 09:37
+ */
+
+namespace Phpfunc\Operation;
+
+
+class Delete
+{
+
+}
